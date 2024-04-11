0e2b5d3f50fa7bf49c88bc04a01f6de969dd8494
WittyMoustache <adrianiperezmartos@gmail.com>
2024-04-11 22:49:22 +0200

    test

--- obj/Debug/net8.0/FirstLearningProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstLearningProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstLearningProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0dde24f5d809c32cc05d34de33d675269bcc3a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f7f8aaf52c2f0b31934a95fa6883cecde9a7b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstLearningProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstLearningProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
